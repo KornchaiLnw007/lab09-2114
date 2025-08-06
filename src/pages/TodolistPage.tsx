496e347dc2fa3d2f7e40d629e3c07694d0aa7e49
KornchaiLnw007 <korrachai.gorn@gmail.com>
2025-08-06 15:03:33 +0700

    commit

--- src/pages/TodolistPage.tsx
+++ src/pages/TodolistPage.tsx
@@ -45,11 +45,14 @@ function App() {
     setTasks(newTasks);
   };
 
+  const AllTasks = tasks.length;
+  const DoneTasks = tasks.filter((task) => task.isDone).length;
+
   return (
     <div className="col-12 m-2 p-0">
       <div className="container text-center">
         <h2>Todo List</h2>
-        <span className="m-2">All : () Done : ()</span>
+        <span className="m-2">All : ({AllTasks}) Done : ({DoneTasks})</span>
         {/* Modal Component */}
         <button
           type="button"
